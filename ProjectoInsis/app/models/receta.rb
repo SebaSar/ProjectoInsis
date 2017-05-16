c45fe2188c0919cbf42348b2d88897af0761be87
Mostojic <mauricio.ostojic@ceinf.cl>
2017-05-16 00:18:38 -0400

    Update receta.rb
    asociacion y validacion modelo receta

--- ProjectoInsis/app/models/receta.rb
+++ ProjectoInsis/app/models/receta.rb
@@ -1,2 +1,9 @@
 class Receta < ApplicationRecord
+  has_many :insumos
+  
+  validates :cod_receta , uniqueness: true
+  validates :nombre, presence: {:message => 'Se necesita nombre de la receta'}
+  validates :nombre, uniqueness: {:message => 'La receta ingresada ya existe'}
+  validates :precio_venta, numericality: true
+  
 end
