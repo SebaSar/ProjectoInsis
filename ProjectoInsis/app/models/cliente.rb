cf64babc6a70ae24efbecb409f95dd7e22fedbea
Sebastian_Sarmiento <sebastian.sarmientoi@utem.cl>
2017-05-16 04:42:59 -0400

    Subiendo validaciones de clases cliente y pedido

--- ProjectoInsis/app/models/cliente.rb
+++ ProjectoInsis/app/models/cliente.rb
@@ -1,2 +1,9 @@
 class Cliente < ApplicationRecord
+	has_many :pedidos
+
+	validates :cod_cli , uniqueness: true
+	validates :nombre , presence: {:message => 'Se necesita nombre de cliente'}
+	validates :nombre , uniqueness: true
+	validates :telefono , numericality: true
+	validates :telefono , uniqueness: true
 end
--- ProjectoInsis/app/models/pedido.rb
+++ ProjectoInsis/app/models/pedido.rb
@@ -1,2 +1,5 @@
 class Pedido < ApplicationRecord
+	belongs_to :cliente
+
+	validates :cod_pedido , uniqueness: true
 end
