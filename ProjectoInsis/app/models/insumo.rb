c3594bd9127e7a48badc1c651204c9972f9a11b3
Mostojic <mauricio.ostojic@ceinf.cl>
2017-05-16 00:12:02 -0400

    Update insumo.rb
    asociacion y validacion modelo insumo

--- ProjectoInsis/app/models/insumo.rb
+++ ProjectoInsis/app/models/insumo.rb
@@ -1,2 +1,18 @@
 class Insumo < ApplicationRecord
+  belongs_to :receta, foreign_key: :cod_receta
+  
+  validates :cod_insumo , uniqueness: true 
+  validates :nombre, presence: {:message => 'Se necesita nombre de producto'}
+  validates :nombre , uniqueness: {:message => 'El producto ingresado ya existe'}
+  validates :cantidad, numericality: true
+  validates :unidad_medida, presence: {:message => 'Ingrese unidad de medida delproducto'}
+  validates :fecha_vencimiento_valida
+   
+  def fecha_vencimiento_valida
+     if fecha_venc.blank? and fecha_venc < Date.today
+       errors.add(:fecha_venc,"Fecha ingresada no es válida")
+     end
+   end
+       
+  
 end
